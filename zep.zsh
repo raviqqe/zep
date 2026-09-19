87a262aea98c1fd83cf30ba19edbcb9614eff1f9
Yota Toyama <raviqqe@gmail.com>
2026-09-18 20:26:13 -0700

    Async Git status (#4)

--- zep.zsh
+++ zep.zsh
@@ -1,31 +1,63 @@
-zstyle :vcs_info:git:* formats '%F{248}%b%f %F{cyan}%m%f'
-zstyle :vcs_info:git*+set-message:* hooks git-remote
+autoload -Uz add-zsh-hook
 
-+vi-git-remote() {
-  local behind ahead
+zstyle :vcs_info:git:* formats '%F{248}%b%f '
+zstyle :vcs_info:git*+post-backend:* hooks git-status
 
-  git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null |
-    read behind ahead ||
-    return
+zep-git-status() {
+  local behind ahead markers
 
-  git fetch --no-write-fetch-head >/dev/null 2>&1 &|
-
-  if [ -n "$hook_com[staged]" -o -n "$hook_com[unstaged]" ]; then
-    hook_com[misc]+=*
+  if ! git diff --quiet || ! git diff --quiet --staged; then
+    markers+=*
   fi
 
-  if [ $ahead -gt 0 ]; then
-    hook_com[misc]+=^
+  if git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null |
+    read behind ahead; then
+    git fetch --no-write-fetch-head >/dev/null 2>&1 &|
+
+    if [ $ahead -gt 0 ]; then
+      markers+=^
+    fi
+
+    if [ $behind -gt 0 ]; then
+      markers+=v
+    fi
   fi
 
-  if [ $behind -gt 0 ]; then
-    hook_com[misc]+=v
-  fi
+  echo "$markers"
+}
 
-  if [ -n "$hook_com[misc]" ]; then
-    hook_com[misc]+=' '
+zep-git-status-cancel() {
+  if [ -n "$zep_git_status_fd" ]; then
+    zle -F $zep_git_status_fd
+    exec {zep_git_status_fd}<&-
+    unset zep_git_status_fd
   fi
 }
+
+zep-git-status-callback() {
+  local markers
+
+  read -r -u $1 markers
+  zep-git-status-cancel
+
+  if [ "$markers" != "$zep_git_status" ]; then
+    zep_git_status=$markers
+    zle reset-prompt
+  fi
+}
+
+zep-git-status-reset() {
+  zep-git-status-cancel
+  zep_git_status=
+}
+
++vi-git-status() {
+  zep-git-status-cancel
+  exec {zep_git_status_fd}< <(zep-git-status)
+  zle -F $zep_git_status_fd zep-git-status-callback
+}
+
+add-zsh-hook chpwd zep-git-status-reset
 
 function zle-keymap-select zle-line-init {
   if [ $KEYMAP = vicmd ]; then
@@ -40,5 +72,5 @@ function zle-keymap-select zle-line-init {
 zle -N zle-keymap-select
 zle -N zle-line-init
 
-PROMPT='%F{blue}%~%f ${vcs_info_msg_0_}%(?..%F{red}[%?]%f )
+PROMPT='%F{blue}%~%f ${vcs_info_msg_0_}%F{cyan}${zep_git_status:+$zep_git_status }%f%(?..%F{red}[%?]%f )
 %F{magenta}$vi_mode%f '
